9d6c2bdea5f13494fb92df0b3ab7d2421e072797
ilbay <ilbay@ilbay-Aspire-5750G.(none)>
2012-01-29 22:56:53 +0200

    startsWith and endsWith methods are added to String class.

--- String.js
+++ String.js
@@ -1,8 +1,26 @@
 String.prototype.insert=function(pos,s){
 	if(pos>=this.length)
-		return this.toStr()+s;
+		return this.valueOf()+s;
 	else if(pos<=0)
-		return s+this.toStr();
+		return s+this.valueOf();
 	else
-		return this.substr(0,pos)+s+this.substr(pos+1);
+		return this.substr(0,pos)+s+this.substr(pos);
+}
+
+String.prototype.startsWith=function(s){
+	if(this.length<s.length)
+		return false;
+	else if(this.substr(0,s.length)==s)
+		return true;
+	else
+		return false;
+}
+
+String.prototype.endsWith=function(s){
+	if(this.length<s.length)
+		return false;
+	else if(this.substr(this.length-s.length)==s)
+		return true;
+	else
+		return false;
 }
