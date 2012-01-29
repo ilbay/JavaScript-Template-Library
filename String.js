46f1e8be7fae8d9258d4968e9b6bb47191a7c306
ilbay <ilbay@ilbay-Aspire-5750G.(none)>
2012-01-29 22:40:51 +0200

    Some errors of string class are updated.

--- String.js
+++ String.js
@@ -1,6 +1,8 @@
 String.prototype.insert=function(pos,s){
-	if(end>=this.length)
+	if(pos>=this.length)
 		return this.toStr()+s;
+	else if(pos<=0)
+		return s+this.toStr();
 	else
 		return this.substr(0,pos)+s+this.substr(pos+1);
 }
